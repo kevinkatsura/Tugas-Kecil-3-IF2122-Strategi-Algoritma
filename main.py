14cac947b2d4c49434dc15a4889b5e6a1dbd9414
kevinkatsura <kevinkatsura@users.noreply.github.com>
2021-04-03 00:42:39 +0700

    menambah sebagian

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tkinter
-import API
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import Graph
 
 window = tkinter.Tk()
 window.title(" Bala-bala ")
@@ -17,13 +21,32 @@ label2 = tkinter.Label(window,text = "Masukkan Posisi Asal")
 label3 = tkinter.Label(window,text = "Masukkan Posisi Tujuan")
 entry2 = tkinter.Entry(window, bd = 5)
 entry3 = tkinter.Entry(window, bd = 5)
-button2 = tkinter.Button(window,text = " Cari Jarak " , command = API.API)
+button2 = tkinter.Button(window,text = " Cari Jarak ")
 
 label2.grid(row=1,column=0)
 entry2.grid(row=1,column = 1)
 label3.grid(row=2,column=0)
 entry3.grid(row=2,column=1)
 button2.grid(row=2 ,column = 2)
+
+# Graph visualization
+f = Figure(figsize=(5,5), dpi=100)
+a = f.add_subplot(111)
+
+
+# Driver code
+G = Graph.GraphVisualization()
+G.addEdge(0, 2)
+G.addEdge(1, 2)
+G.addEdge(1, 3)
+G.addEdge(5, 3)
+G.addEdge(3, 4)
+G.addEdge(1, 0)
+G.visualize()
+
+canvas = FigureCanvasTkAgg(f,G)
+canvas.show()
+canvas.get_tk_widget().grid(row=3 , column = 0, fill = tkinter.BOTH, expand = True)
 
 window.mainloop()
 
