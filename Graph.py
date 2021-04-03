4c31bbac7adbfaff68f429f3e64491b8cf6f4ea7
kevinkatsura <kevin.sitanggang55@gmail.com>
2021-04-03 07:47:52 +0700

    graph

--- Graph.py
+++ Graph.py
@@ -7,23 +7,16 @@ x = [0,3]
 # Defining a Class
 class GraphVisualization:
 
+    # Inisialisasi atribut visual dengan array kosong
     def __init__(self):
-        # visual is a list which stores all
-        # the set of edges that constitutes a
-        # graph
         self.visual = []
 
-    # addEdge function inputs the vertices of an
-    # edge and appends it to the visual list
+    # Menambahkan edge
     def addEdge(self, a, b):
         temp = [a, b]
         self.visual.append(temp)
 
-    # In visualize function G is an object of
-    # class Graph given by networkx G.add_edges_from(visual)
-    # creates a graph with a given list
-    # nx.draw_networkx(G) - plots the graph
-    # plt.show() - displays the graph
+    #Visualisasi
     def visualize(self):
         G = nx.Graph()
         G.add_edges_from(self.visual)
